29da2b5ce4cfd14c4d9adc5b8718667b2d59e5a9
Viktoriia <viktoria.murzhak@gmail.com>
2022-10-14 00:45:13 +0300

    конспект відно 1 модуль 1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+//
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const elem1 = React.createElement('span', { children: 'hola!' });
+// const elem2 = React.createElement('span', { children: 'mmmolla!' });
+
+// const element = React.createElement('div', {
+//   a: 5,
+//   b: 10,
+//   children: [elem1, ' ', elem2],
+// });
+// console.log('element', element);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(element);
+
+// ! Переписуємо на js xml (JSX)
+
+// const elem1 = <span>hello </span>;
+// const elem2 = <span>mello</span>;
+
+// const jsxElement = (
+//   <div>
+//     {elem1}
+//     {elem2}
+//   </div>
+// );
+
+// console.log(jsxElement);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(jsxElement);
+
+// ! Створюємо компонент
+
+// const painting = (
+//   <div>
+//     <img src="" alt="" width="480" />
+//     <h2></h2>
+//     <p>
+//       Автор: <a href=""> </a>
+//     </p>
+//     <p>Цена: кредитов</p>
+//     <p>Доступность: 'заканчивается или есть в наличии</p>
+//     <button type="button">Добавить в корзину</button>
+//   </div>
+// );
+
+// console.log('painting', painting);
+
+//! це було в домашці
+// ! =========================================
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { App } from 'components/App';
+// import './index.css';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
